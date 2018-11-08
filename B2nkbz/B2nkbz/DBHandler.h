4636a2fc1737ef3534cfaf56fe448d376fc8d86b
WaveletGreen <1243093769@qq.com>
2018-11-08 09:27:32 +0800

    Update DBHandler.h

--- B2nkbz/B2nkbz/DBHandler.h
+++ B2nkbz/B2nkbz/DBHandler.h
@@ -4,7 +4,7 @@
 #define NUM 100
 #define USERNAME "B2nkbz"
 #define PASSWORD "B2nkbz"
-#define DBNAME "tc11"
+#define DBNAME "localhost/tc11"
 using namespace std;
 int B2nkbz_save(char *oraUser, char *pwd, char *address, //map<string, int> &parts_vec,
 	int status);
